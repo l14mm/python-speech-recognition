9c727c19fa0f0f37809d8a9eb9b455d5248a9cc8
liammoroney <liam.moroney14@gmail.com>
2019-02-23 12:38:50 +0000

    Added error handling. Changed to listen for audio in the background, when it detects words it will append them to a file with a timestamp and save the audio in a wav file.

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 import speech_recognition as sr
+import time
+from datetime import datetime
+
+def callback(recognizer, audio):
+    try:
+        timestamp = datetime.now()
+        text = recognizer.recognize_google(audio)
+        print("Google Speech Recognition thinks you said " + text)
+        
+        with open("microphone-results.wav", "wb") as f:
+            f.write(audio.get_wav_data())
+
+        with open("text2speed-results.txt", "a") as f:
+            f.write(f'{timestamp} - {text}\n')
+
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
 if __name__ == "__main__":
     r = sr.Recognizer()
+    m = sr.Microphone()
     
-    with sr.Microphone() as source:
-        r.adjust_for_ambient_noise(source)
-        print('Say something')
-        audio = r.listen(source)
+    stop_listening = r.listen_in_background(m, callback)
 
-        try:
-            text = r.recognize_google(audio)
-            print(text)
-        except:
-            print('error')
+    time.sleep(30)
+
+    stop_listening(wait_for_stop=False)
+    
